aceb0513a657f173ac28761cfaa7a66aa9b80d78
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2018-12-02 01:08:16 +0200

    added frontend note service

--- frontend/src/app/services/notes.service.ts
+++ frontend/src/app/services/notes.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NotesService {
 
-  constructor() { }
+export class NotesService {
+  public notes =[];
+   token = localStorage.getItem('Authentication');
+  constructor(private http: HttpClient) { }
+
+  getNotes(){
+    //token 
+    this.http.get('http://localhost:80/note/getNotes', this.token).subscribe((res: any) => {
+     this.notes = res.data;
+
+    });
+  }
+
+  createNote(titleIn,descriptionIn){
+    var body = {
+      title: titleIn,
+      description: descriptionIn,
+    }
+    this.http.post(appConfig.apiUrl + 'http://localhost:80/note/createNote' ,this.token, body)
+    .subscribe(res => {
+       //worth it?
+       this.getNotes();
+    });
+  }
+  
+  deleteNote(noteID) {
+    this.http.delete('http://localhost:80/note/deleteNote/:' +noteID,this.token)
+      .subscribe(res => {
+        //worth it?
+        this.getNotes();
+    });
+  }
+
+  updateNote(noteID,titleIn,descriptionIn){
+    var body = {
+      title: titleIn,
+      description: descriptionIn,
+    }
+    this.http.patch(appConfig.apiUrl + 'http://localhost:80/note/updateNote/:' +noteID,this.token , body)
+    .subscribe(res => {
+       //worth it?
+       this.getNotes();
+    });
+  }
 }
