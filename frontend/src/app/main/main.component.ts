3e1ef62be7791ff932f0a10f60791db68f164c6a
Mahmoud Saleh <mmsmhh@Gmail.com>
2018-12-02 01:48:53 +0200

    delete services and copy the code to the components

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NotesService } from '../services/notes.service';
-import { WeatherService } from '../services/weather.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -9,9 +8,68 @@ import { WeatherService } from '../services/weather.service';
 })
 export class MainComponent implements OnInit {
 
-constructor(private notes : NotesService,private weather : WeatherService) { }
+constructor(private http: HttpClient) { }
+token = localStorage.getItem('Authentication');
+public notes =[];
+
+httpOptions = {
+headers: new HttpHeaders({
+  'Content-Type':  'application/json',
+  'Authorization': this.token
+})
+};
+
 
   ngOnInit() {
+    this.getNotes();
+    console.log(this.notes)
+  }
+
+
+
+
+  getNotes(){
+    //token
+    this.http.get('http://localhost:80/note/getNotes', this.httpOptions).subscribe((res: any) => {
+     this.notes = res.data;
+    });
+  }
+
+  createNote(titleIn,descriptionIn){
+    var body = {
+      title: titleIn,
+      description: descriptionIn,
+    }
+    this.http.post('http://localhost:80/note/createNote' ,body,this.httpOptions)
+    .subscribe(res => {
+       //worth it?
+       console.log(res)
+       this.getNotes();
+    });
+  }
+
+  deleteNote(noteID) {
+    this.http.delete('http://localhost:80/note/deleteNote/:' +noteID,this.httpOptions)
+      .subscribe(res => {
+        //worth it?
+        console.log(res)
+
+        this.getNotes();
+    });
+  }
+
+  updateNote(noteID,titleIn,descriptionIn){
+    var body = {
+      title: titleIn,
+      description: descriptionIn,
+    }
+    this.http.patch('http://localhost:80/note/updateNote/:' +noteID,body,this.httpOptions)
+    .subscribe(res => {
+       //worth it?
+       console.log(res)
+
+       this.getNotes();
+    });
   }
 
 }
