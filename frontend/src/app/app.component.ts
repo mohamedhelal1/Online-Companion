55fc9be29ca5558fcbf24648d42afb493a9aec72
mohamedHelal <m.h.helal1997@gmail.com>
2018-12-01 15:52:22 +0200

    added Dockerfile for frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-        <router-outlet (activate)="onActivate($event)"></router-outlet>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Scroll up to the top of the page on changing the route
-  onActivate(event) {
-    window.scroll(0, 0);
-  }
+  title = 'app';
 }
