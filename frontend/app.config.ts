f1519f357aba1dbc219db73df8b9fbaf60e66991
Mahmoud Saleh <mmsmhh@Gmail.com>
2018-12-02 14:57:57 +0200

    change backend port

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,4 +1,4 @@
 export const appConfig = {
-    backendUrl: 'http://localhost:80/',
+    backendUrl: 'http://localhost:3000/',
     googleClientId:'1056543646824-gj2qrem1jjrq55vmjo86adrqnip6aol5.apps.googleusercontent.com'
   };
